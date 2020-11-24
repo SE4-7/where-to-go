1f24bd6dab69d2cfa6b7261ffd513ced779c251c
Yang Cheol Joo <rlaengks0001@naver.com>
2020-11-24 11:28:26 +0900

    Rename workspace app to android

---
+++ android/app/src/main/java/com/example/wheretogo/MapActivity.kt
@@ -0,0 +1,37 @@
+package com.example.wheretogo
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.pm.Signature
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Base64
+import android.view.ViewGroup
+import net.daum.mf.map.api.MapView
+import java.security.MessageDigest
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val mapView = MapView(this)
+        val mapViewContainer: ViewGroup = findViewById(R.id.mapView)
+        mapViewContainer.addView(mapView)
+    }
+}
+
+// App Hash Key
+fun getSignature(context: Context): String? {
+    val packageManager = context.packageManager
+    val packageInfo = packageManager.getPackageInfo(context.packageName, PackageManager.GET_SIGNATURES)
+
+    for (signature: Signature in packageInfo.signatures) {
+        val messageDigest = MessageDigest.getInstance("SHA")
+        messageDigest.update(signature.toByteArray())
+        return Base64.encodeToString(messageDigest.digest(), Base64.DEFAULT)
+    }
+
+    return null
+}
